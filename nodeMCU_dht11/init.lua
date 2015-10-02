e3262949dfef35357381c06e588ef14b976065f1
TECHNOSMM <samiunlu10@hotmail.com>
2015-10-02 15:28:54 +0300

    Update init.lua

--- nodeMCU_dht11/init.lua
+++ nodeMCU_dht11/init.lua
@@ -36,7 +36,7 @@ conn=net.createConnection(net.TCP, 0)
 conn:on("receive", function(conn, payload) print(payload) end)
 -- api.thingspeak.com 184.106.153.149
 conn:connect(80,'184.106.153.149') 
-conn:send("GET /update?key=YX4WMGSGS5A31Y1M&field1="..temp.."."..temp_decimial.."&field2="..humi.."."..humi_decimial.." HTTP/1.1\r\n") 
+conn:send("GET /update?key=XXXXXXXXXX&field1="..temp.."."..temp_decimial.."&field2="..humi.."."..humi_decimial.." HTTP/1.1\r\n") 
 conn:send("Host: api.thingspeak.com\r\n") 
 conn:send("Accept: */*\r\n") 
 conn:send("User-Agent: Mozilla/4.0 (compatible; esp8266 Lua; Windows NT 5.1)\r\n")
